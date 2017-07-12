9dcbbd0b01fd7dfe16856bab6674c0143e417429
Ciarán Mulholland <ciaranmul@googlemail.com>
2017-07-12 17:32:21 +0100

    Import initial code and assets Imports assets:
    * Car (and material)
    
    * Pillar (and material)
    
    * Test scene
    
    Imports scripts:
    
    * car movement

---
+++ Assets/car/car_movement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class car_movement : MonoBehaviour {
+
+    public int player = 0;
+    public float acceleration = 10f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void FixedUpdate () {
+        if (Input.GetAxis("Accelerate") != 0)
+        {
+            Debug.Log("accelerating");
+            GetComponent<Rigidbody>().AddForce(transform.forward * acceleration);
+        }
+
+        if (Input.GetButton("Break"))
+        {
+            Debug.Log("breaking");
+        }
+		
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            Debug.Log("Horizontal Movement");
+        }
+		
+	}
+}
