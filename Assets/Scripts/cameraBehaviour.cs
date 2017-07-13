ee721a8c1efc3daf62cbf1d4fa9af238d3e085f4
Ciarán Mulholland <ciaranmul@googlemail.com>
2017-07-13 22:29:51 +0100

    Add Title scene, Add car movement controls * Adds Title scene - from unity game jam package
        * Game can be started from title screen
    
        * Starting game changes music and scene (currently to test scene)
    
    * Adds movement controls for single car
    
        * Using raycasting for terrain detection - exerts upwards force when triggered
    
        * Automatically levels out when not grounded
    
    * Resetting inputs; adding accelerate axis
    
        * accelerate mapped to controller triggers (axis 3)

---
+++ Assets/Scripts/cameraBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraBehaviour : MonoBehaviour {
+
+    private Camera mainCamera;
+    public GameObject target;
+    public float cameraDistance = 20;
+
+	// Use this for initialization
+	void Start () {
+        mainCamera = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 targetPos = target.transform.transform.position;
+        mainCamera.transform.position = new Vector3(targetPos.x, targetPos.y + cameraDistance, targetPos.z - cameraDistance/5);
+	}
+}
