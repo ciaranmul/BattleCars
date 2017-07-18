277530efa193c6d3e4330661082c6c9c570c1a24
Ciarán Mulholland <ciaranmul@googlemail.com>
2017-07-18 14:57:35 +0100

    Add weapon rotation
    * Allows players' weapons to be rotated using the right stick
    * Had to rescale the car body as it was causing child objects to distort
    when moved.
    
    See Issue #8

--- Assets/car/CarWeapon.cs
+++ Assets/car/CarWeapon.cs
@@ -4,21 +4,56 @@ using UnityEngine;
 
 public class CarWeapon : MonoBehaviour {
 
+    float deadZone = 0.1f;
+
+    float rotationSpeed = 4f;
+
+    float turnValueHorizontal;
+    float turnValueVertical;
 
     // Use this for initialization
     void Start () {
-		
+        		
 	}
 	
 	// Update is called once per frame
 	void Update () {
         // TODO: Get rotation of right stick and point the weapon that way.
         //       Weapon should rotate relative to the orientation of the main camera.
-		if (Input.GetButton(this.GetComponent<CarGenerateInputStrings>().fire1))
+        turnValueHorizontal = 0.0f;
+        turnValueVertical = 0.0f;
+
+        float turnAxisHorizontal = Input.GetAxis(this.GetComponentInParent<CarGenerateInputStrings>().horizontalAim);
+        float turnAxisVertical = Input.GetAxis(this.GetComponentInParent<CarGenerateInputStrings>().verticalAim);
+
+        if (Mathf.Abs(turnAxisHorizontal) > deadZone)
+        {
+            Debug.Log("horizontal movement detected");
+            turnValueHorizontal = turnAxisHorizontal;
+        }
+
+        if(Mathf.Abs(turnAxisVertical) > deadZone)
+        {
+            Debug.Log("vertical movement detected");
+            turnValueVertical = turnAxisVertical;
+        }
+
+
+		if (Input.GetButton(this.GetComponentInParent<CarGenerateInputStrings>().fire1))
         {
             // Fire projectile now
-            Debug.Log(string.Format("Player {0} fired", this.GetComponent<CarGenerateInputStrings>().player));
+            Debug.Log(string.Format("Player {0} fired", this.GetComponentInParent<CarGenerateInputStrings>().player));
         }
 	}
-   
+
+    private void FixedUpdate()
+    {
+        float rotationAngle = Mathf.Atan2(turnValueHorizontal, -turnValueVertical) * Mathf.Rad2Deg;
+        Debug.Log(rotationAngle);
+        if (Mathf.Abs(turnValueVertical) != 0 || Mathf.Abs(turnValueHorizontal) != 0)
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0.0f, rotationAngle, 0.0f), rotationSpeed * Time.deltaTime);
+        }
+    }
+
 }
