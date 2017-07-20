652a03e31206901bb706bdcda25a4ab22da057cd
Ciarán Mulholland <ciaranmul@googlemail.com>
2017-07-20 18:27:38 +0100

    Add option for number of players to title menu.
    * Adds a drop-down menu to select number of players.
    * Adds script to eventListener to listen for change in drop-down option.
    * Adds scene manager to test-scene to spawn necessary player objects.
    * Adds script to scene manager to spawn players according to drop-down
    option.
    
    See Issue #7

---
+++ Assets/Game Jam Template/Scripts/SetNumberOfPlayers.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetNumberOfPlayers : MonoBehaviour {
+
+
+    public Dropdown playerChoice;
+
+	// Use this for initialization
+	void Start () {
+        playerChoice.onValueChanged.AddListener(delegate
+        {
+            DropDownChange(playerChoice);
+        });
+    }
+	
+    void DropDownChange(Dropdown target)
+    {
+        PlayerPrefs.SetInt("players", target.value + 2);
+        Debug.Log(string.Format("players = {0}", PlayerPrefs.GetInt("players")));
+    }
+
+    void Destroy()
+    {
+        playerChoice.onValueChanged.RemoveAllListeners();
+    }
+
+}
