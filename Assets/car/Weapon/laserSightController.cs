44b8a7884e76c0b34d47b64356d520cb0c45fa13
Ciarán Mulholland <ciaranmul@googlemail.com>
2017-07-20 16:22:09 +0100

    Add dust trails; Make lasers dynamic; Beautify
    * Adds dust trails following cars
    * Adds Raycast to laser sights so they don't shine through objects
    * Beautifies bullet impact particle system

---
+++ Assets/car/Weapon/laserSightController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class laserSightController : MonoBehaviour {
+
+    Ray lineOfSight;
+    RaycastHit hit;
+
+    private LineRenderer laser;
+    public LayerMask collisionMask;
+    public Transform spawn;
+    public float range = 100;
+
+    // Use this for initialization
+    void Start () {
+        lineOfSight = new Ray(spawn.position, spawn.forward);
+        if ( GetComponent<LineRenderer>() )
+        {
+            laser = GetComponent<LineRenderer>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        lineOfSight.origin = spawn.position;
+        lineOfSight.direction = spawn.forward;
+        Debug.DrawRay(lineOfSight.origin, range * lineOfSight.direction, Color.red, 1);
+
+        if (Physics.Raycast(lineOfSight, out hit, range, collisionMask))
+        {
+            laser.SetPosition(1, Vector3.forward * hit.distance);
+        }
+        else
+        {
+            laser.SetPosition(1, Vector3.forward * range);
+        }
+        
+	}
+}
