a9fdd21bde5b040a86f852f27365d1f51f415b2a
Ciarán Mulholland <ciaranmul@googlemail.com>
2017-07-21 10:17:33 +0100

    Fix player selection
    * Players now spawned by iterating through array
    * Player guis now also spawned by scene manager
    
    See Issue #7

---
+++ Assets/Scripts/EnablePlayers.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnablePlayers : MonoBehaviour
+{
+
+    public GameObject[] players;
+    public GameObject[] uis;
+    private int numPlayers;
+
+    // Use this for initialization
+    void Start()
+    {
+        numPlayers = PlayerPrefs.GetInt("players");
+        spawnPlayers();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void spawnPlayers()
+    {
+        Debug.Log(string.Format("PlayerPrefs.players = {0}", numPlayers));
+        for (int i = 0; i < numPlayers; i++)
+        {
+            players[i].SetActive(true);
+            uis[i].SetActive(true);
+        }
+    }
+}
